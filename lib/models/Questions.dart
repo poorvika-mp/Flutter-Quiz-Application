0e6d1dcb24e69fc920a52a60103062669d8b91e6
poorvika-mp <57279983+poorvika-mp@users.noreply.github.com>
2021-12-05 13:42:35 +0530

    Add files via upload

---
+++ lib/models/Questions.dart
@@ -0,0 +1,35 @@
+class Question {
+  final int id, answer;
+  final String question;
+  final List<String> options;
+
+  Question({this.id, this.question, this.answer, this.options});
+}
+
+const List sample_data = [
+  {
+    "id": 1,
+    "question":
+        "Flutter is an open-source UI software development kit created by ______",
+    "options": ['Apple', 'Google', 'Facebook', 'Microsoft'],
+    "answer_index": 1,
+  },
+  {
+    "id": 2,
+    "question": "When did google release Flutter.",
+    "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
+    "answer_index": 2,
+  },
+  {
+    "id": 3,
+    "question": "Which is the capital city of Karnataka. ",
+    "options": ['Mysore', 'Chennai', 'Bangalore', 'Panaji'],
+    "answer_index": 2,
+  },
+  {
+    "id": 4,
+    "question": "Who is the current Prime Minister of India?",
+    "options": ['Indira Gandhi', 'Manmohan Singh', 'Narendra Modi', 'Rahul Gandhi'],
+    "answer_index": 2,
+  },
+];
